a15d860d1ddf4b1ec6bfdc4d6f6f07c3161aa954
Trinh Gliedt <chuot2008@gmail.com>
2020-12-23 22:16:36 -0800

    navbar 2/4

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,12 +1,11 @@
 import React, { useState } from 'react';
 import styled from 'styled-components';
-import logo from '../images/logo_dark.JPG';
+import logo from '../images/EquityX_Logo.png';
 
 const NavBarContainer = styled.div`
-    width: 80%;
+    width: 100%;
     display: flex;
     justify-content: left;
-
     @media only screen and (max-width: 768px) {
         width: 100%;
         display: flex;
@@ -15,12 +14,22 @@ const NavBarContainer = styled.div`
 `;
 
 const Logo = styled.div`
-    padding: 0.1em 0.2em;
-    width: 8em;
-
+    padding: 0 0.2em;
+    width: 10em;
+    align-self: flex-end;
     @media only screen and (max-width: 768px) {
         width: 5em;
         margin-right: 0.5%;
+    }
+`;
+
+const Demo = styled.div`
+    padding: 0 0.2em;
+    width: 10em;
+    align-self: center;
+    @media only screen and (max-width: 768px) {
+        width: 5em;
+        margin-left: 0.5%;
     }
 `;
 
@@ -28,10 +37,11 @@ const TabContainer = styled.div`
     height: 100%;
     width: 90%;
     display: flex;
+    align-self: flex-end;
     justify-content: center;
     flex-wrap: wrap;
     flex-direction: row;
-
+    
     @media only screen and (max-width: 768px) {
         width: 100%;
     }
@@ -40,30 +50,28 @@ const NavBarTab = styled.div`
     height: 100%;
     display: flex;
     align-items: flex-end;
-    padding: 1% 1%;
+    padding: 1% 2.5%;
     justify-content: center;
     margin: 0 0.5%;
-
+    
     @media only screen and (max-width: 768px) {
     }
 
     :hover ~ #productDropdown {
-        transition: .4s ease-out 0s; 
-        display: block;
+        display: none;
       }
 
     
 `;
 
 const TabLabel = styled.div`
-    padding-bottom: 1em;
     height: 100%;
     display: flex;
     align-items: flex-end;
     justify-content: center;
-    
-    font-size: 1em;
+    font-size: 1.1em;
     cursor: pointer;
+    
 
 
     @media only screen and (max-width: 768px) {
@@ -71,14 +79,20 @@ const TabLabel = styled.div`
     }
     
 `;
-
 const DropDownContent = styled.div`
-    display: none;
-    position: absolute;
-    font-size: 0.5em;
+    width: 42em;
+    margin-left: 20%;
     background-color: white;
+    position: absolute;
+    font-size: 1em;
+    border-top: 1px solid grey;
     box-shadow: 0px 8px 16px 0px rgba(0,0,0,0.2);
     z-index: 1;
+    
+    @media only screen and (max-width: 768px) {
+        font-size: 0.5em;
+    }
+    
 `;
 
 const NavBar = ({ setSelectedTab }) => {
@@ -86,6 +100,33 @@ const NavBar = ({ setSelectedTab }) => {
     const [toolSelected, setToolSelected] = useState(false);
     const [tutorialSelected, setTutorialSelected] = useState(false);
     const [adminSelected, setAdminSelected] = useState(false);
+    const [isShown_productDropdown, setIsShown_productDropdown] = useState(false);
+    const [isShown_resourceDropdown, setIsShown_resourceDropdown] = useState(false);
+
+    const handleMouseEnter = (tab) => {
+        if (tab == "products"){
+            setIsShown_productDropdown(true)
+        }
+        else if (tab == "resources"){
+            setIsShown_resourceDropdown(true)
+        }
+    };
+
+    const handleMouseLeave= (tab, tabContent=null) => {
+        if (tab == "products" && tabContent == null){
+            setIsShown_productDropdown(false)
+        }
+        else if (tab == "products" && tabContent == 'productDropDownContent'){
+            setIsShown_productDropdown(true)
+        }
+        else if (tab == "resources" && tabContent == null){
+            setIsShown_resourceDropdown(false)
+        }
+        else if (tab == "resources" && tabContent == 'resourceDropDownContent'){
+            setIsShown_resourceDropdown(true)
+        }
+     };
+
 
     const handleClick = (tab) => {
         setAboutSelected(false);
@@ -100,46 +141,30 @@ const NavBar = ({ setSelectedTab }) => {
         // setSelectedTab(tab);
     };
     return (
+        <>
         <NavBarContainer>
             <Logo><img src={logo} alt="logo" width="100%"/></Logo>
             <TabContainer>
                 <NavBarTab 
-                    id = "products">
+                    id = "products"
+                    onMouseEnter={() => handleMouseEnter('products')}
+                    onMouseLeave={() => handleMouseLeave('products')}
+                    >
                     <TabLabel 
                         
                         className="tabLabel"
                         onClick={() => handleClick('about')}
                         selected={aboutSelected}
+                        
                     >
                         Products & Solutions
                     </TabLabel>
                 </NavBarTab>
-                <DropDownContent
-                    id = "productDropdown"
-                    >
-                    <table>
-                        <thead>
-                            <th>Private Companies</th>
-                            <th>Investors</th>
-                        </thead>
-                        <tr>
-                            <td>Overview</td>
-                            <td>Overview</td>
-                            <td></td>
-                        </tr>
-                        <tr>
-                            <td>Cap Table Management</td>
-                            <td>Cap Table Management</td>
-                            <td></td>
-                        </tr>
-                        <tr>
-                            <td>Scenario Modeling</td>
-                            <td></td>
-                            <td></td>
-                        </tr>
-                    </table>
-                </DropDownContent>
-                <NavBarTab>
+                
+                <NavBarTab
+                onMouseEnter={() => handleMouseEnter('resources')}
+                onMouseLeave={() => handleMouseLeave('resources')}
+                >
                     <TabLabel
                         className="tabLabel"
                         onClick={() => handleClick('about')}
@@ -163,20 +188,93 @@ const NavBar = ({ setSelectedTab }) => {
                         onClick={() => handleClick('about')}
                         selected={aboutSelected}
                     >
-                        Resources
-                    </TabLabel>
-                </NavBarTab>
-                <NavBarTab>
-                    <TabLabel
-                        className="tabLabel"
-                        onClick={() => handleClick('about')}
-                        selected={aboutSelected}
-                    >
                         About us
                     </TabLabel>
                 </NavBarTab>
             </TabContainer>
+        <Demo>
+            <a href="/">Request a Demo</a>
+        </Demo>
         </NavBarContainer>
+        {isShown_productDropdown && (
+            <DropDownContent
+            id = "productDropdown"
+            onMouseEnter={() => handleMouseLeave('products', 'productDropDownContent')}
+            onMouseLeave={() => handleMouseLeave('products')}
+            >
+                <table>
+                    <thead>
+                        <tr>
+                            <th><a href="/">Private Companies</a></th>
+                            <th><a href="/">Investors</a></th>
+                            <th><a href="/">Public Companies</a></th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td><a href="/">Overview</a></td>
+                            <td><a href="/">Overview</a></td>
+                            <td><a href="/">Overview</a></td>
+                        </tr>
+                        <tr>
+                            <td><a href="/">Cap Table Management</a></td>
+                            <td><a href="/">Fund Administration</a></td>
+                            <td><a href="/">ESPP</a></td>
+                        </tr>
+                        <tr>
+                            <td><a href="/">Scenario Modeling</a></td>
+                            <td><a href="/">Capital Call Line of Credit</a></td>
+                            <td><a href="/">Transfer Agent</a></td>
+                        </tr>
+                        <tr>
+                            <td><a href="/">Private Company Liquidity</a></td>
+                            <td><a href="/">Portfolio Insights</a></td>
+                            <td><a href="/"></a></td>
+                        </tr>
+                    </tbody>
+                </table>
+            </DropDownContent>
+        )}
+        {isShown_resourceDropdown && (
+            <DropDownContent
+            id = "resourceDropdown"
+            onMouseEnter={() => handleMouseLeave('resources', 'resourceDropDownContent')}
+            onMouseLeave={() => handleMouseLeave('resources')}
+            >
+                <table id="resourceDropdownTable">
+                    <thead>
+                        <tr>
+                            <th><a href="/">EquityX Resouces</a></th>
+                            <th><a href="/">Blog</a></th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td><a href="/">Customer Testimonials</a></td>
+                            <td><a href="/">Blog</a></td>
+                        </tr>
+                        <tr>
+                            <td><a href="/">Events & Webinars</a></td>
+                            <td><a href="/">Partner Stories</a></td>
+                        </tr>
+                        <tr>
+                            <td><a href="/">Supports</a></td>
+                            <td><a href="/">Product Education</a></td>
+                        </tr>
+                        <tr>
+                            <td><a href="/">Product Release Notes</a></td>
+                            <td><a href="/">How We Work</a></td>
+                        </tr>
+                    </tbody>
+                </table>
+            </DropDownContent>
+        )}
+        <div>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi tempora distinctio dolore nisi repellat ea iure fugit, magnam laboriosam animi corrupti vero ex possimus reiciendis beatae velit rerum fuga ut!. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad non totam debitis nesciunt aliquam, similique alias, quia tempore recusandae accusamus minus corrupti iure necessitatibus repellat provident nisi sequi amet ullam?</p>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi tempora distinctio dolore nisi repellat ea iure fugit, magnam laboriosam animi corrupti vero ex possimus reiciendis beatae velit rerum fuga ut!. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad non totam debitis nesciunt aliquam, similique alias, quia tempore recusandae accusamus minus corrupti iure necessitatibus repellat provident nisi sequi amet ullam?</p>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi tempora distinctio dolore nisi repellat ea iure fugit, magnam laboriosam animi corrupti vero ex possimus reiciendis beatae velit rerum fuga ut!. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad non totam debitis nesciunt aliquam, similique alias, quia tempore recusandae accusamus minus corrupti iure necessitatibus repellat provident nisi sequi amet ullam?</p>
+        </div>
+    </>
     );
 };
 

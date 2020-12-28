10d77adadca54b35558e840dd7968afd89cf278f
Trinh Gliedt <chuot2008@gmail.com>
2020-12-28 15:16:54 -0800

    moved files to client

--- src/config/colorConfig.js
+++
@@ -1,8 +0,0 @@
-const colors = {
-    navbarBg: 'linear-gradient(rgba(133, 145, 175, 0.637), rgba(159, 179, 235, 0.644), rgba(133, 145, 175, 0.637))',
-    dropdownMenuBg: 'linear-gradient(rgb(133, 145, 175), rgb(159, 179, 235), rgba(133, 145, 175))',
-    containerBg: 'radial-gradient(rgb(133, 145, 175), rgba(131, 146, 187, 0.781), rgb(133, 145, 175))',
-    gold: 'linear-gradient(rgb(158, 134, 80), rgb(228, 216, 189), rgb(158, 134, 80))',
-};
-
-export default colors;

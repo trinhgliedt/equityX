338b19035a2d89cd7d7549fa8a144a2878bf9e0f
Trinh Gliedt <chuot2008@gmail.com>
2020-12-28 22:03:53 -0800

    run build

--- client/src/views/_02_sample.jsx
+++ client/src/views/_02_sample.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
-import CoderImg from "../images/coderImg.png"
+// import CoderImg from "../images/coderImg.png"
 
 const SamplePage = () => {
     
     return (
         <div className="sampleContainer p2 pb30">
             <div className="rowFlex rowCenterContent">
-                <img id="coderImg" src={CoderImg} />
+                {/* <img id="coderImg" src={CoderImg} /> */}
             </div>
             <div className="rowFlex rowCenterContent">
                 <p className="centerText">All the links in the home page are for demonstration purpose only and detail pages are not yet built. It can be done per your request.</p>

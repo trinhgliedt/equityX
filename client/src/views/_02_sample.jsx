043fa65d050f2d059408e7dc3e397b1cabd281e6
Trinh Gliedt <chuot2008@gmail.com>
2020-12-28 23:41:12 -0800

    Fixed coder image for responsive design

--- client/src/views/_02_sample.jsx
+++ client/src/views/_02_sample.jsx
@@ -1,18 +1,19 @@
 import React from 'react';
 import CoderImg from "../images/coderImg.png"
+import { Link } from "@reach/router";
 
 const SamplePage = () => {
     
     return (
         <div className="sampleContainer p2 pb30">
             <div className="rowFlex rowCenterContent">
-                <img id="coderImg" src={CoderImg} alt="Coder" />
+                <img id="coderImg" src={CoderImg}   height="100%" alt="Coder" />
             </div>
             <div className="rowFlex rowCenterContent">
                 <p className="centerText">All the links in the home page are for demonstration purpose only and detail pages are not yet built. It can be done per your request.</p>
             </div>
             <div className="rowFlex rowCenterContent">
-                <a href="/" className="blueFont bold">Back to home page</a>
+                <Link to="/" className="blueFont bold">Back to home page</Link>
             </div>
         </div>
     );

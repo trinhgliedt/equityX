e435f2af14ec51fddbbda18c624d3b8b3bb6f5f5
Trinh Gliedt <chuot2008@gmail.com>
2021-04-27 12:54:57 -0700

    added gh pages

--- client/src/views/_02_sample.jsx
+++ client/src/views/_02_sample.jsx
@@ -1,23 +1,28 @@
-import React, { useEffect} from 'react';
-import CoderImg from "../images/coderImg.png"
+import React, { useEffect } from "react";
+import CoderImg from "../images/coderImg.png";
 import { Link } from "@reach/router";
 
 const SamplePage = () => {
-    useEffect(() => {
-        window.scrollTo({ top: 0, behavior: 'smooth' });
-    }, []);
-    return (
-        <div className="sampleContainer p2 pb10">
-            <div className="rowFlex rowCenterContent">
-                <img id="coderImg" src={CoderImg}   height="100%" alt="Coder" />
-            </div>
-            <div className="rowFlex rowCenterContent">
-                <p className="centerText smallFont">All the links in the home page are for demonstration purpose only and detail pages are not yet built. It can be done per your request.</p>
-            </div>
-            <div className="rowFlex rowCenterContent">
-                <Link to="/" className="smallFont blueFont bold">Back to home page</Link>
-            </div>
-        </div>
-    );
+  useEffect(() => {
+    window.scrollTo({ top: 0, behavior: "smooth" });
+  }, []);
+  return (
+    <div className="sampleContainer p2 pb10">
+      <div className="rowFlex rowCenterContent">
+        <img id="coderImg" src={CoderImg} height="100%" alt="Coder" />
+      </div>
+      <div className="rowFlex rowCenterContent">
+        <p className="centerText smallFont">
+          All the links in the home page are for demonstration purpose only and
+          detail pages are not built.{" "}
+        </p>
+      </div>
+      <div className="rowFlex rowCenterContent">
+        <Link to="/" className="smallFont blueFont bold">
+          Back to home page
+        </Link>
+      </div>
+    </div>
+  );
 };
 export default SamplePage;
